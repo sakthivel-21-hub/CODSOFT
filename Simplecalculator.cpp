d872e148edfa18b893b5f00c58089f305b3f3ba6
SAKTHIVEL V <25cb048@rmkec.ac.in>
2026-01-28 23:01:08 +0530

    Refactor calculator into a class-based structure

--- Simplecalculator.cpp
+++ Simplecalculator.cpp
@@ -1,33 +1,74 @@
 #include <iostream>
+#include <string>
 using namespace std;    
-int main() {    
-    char op;    
-    float num1, num2;    
-    cout << "Enter operator (+, -, *, /): ";    
-    cin >> op;    
-    cout << "Enter two operands: ";    
-    cin >> num1 >> num2;    
-    
-    switch(op) {    
-        case '+':    
-            cout << num1 << " + " << num2 << " = " << num1 + num2;    
-            break;    
-        case '-':    
-            cout << num1 << " - " << num2 << " = " << num1 - num2;    
-            break;    
-        case '*':    
-            cout << num1 << " * " << num2 << " = " << num1 * num2;    
-            break;    
-        case '/':    
-            if(num2 != 0)    
-                cout << num1 << " / " << num2 << " = " << num1 / num2;    
-            else    
-                cout << "Error! Division by zero.";    
-            break;    
-        default:    
-            cout << "Error! Operator is not correct";    
-            break;    
-    }    
-    
-    return 0;  
+class calculator{
+    private:
+        double num1 , num2;
+    public:
+       void inputdata(double a, double b){
+        num1=a;
+        num2=b;
+      }
+       double Addition(){
+        return num1+num2;
+          }
+       double Subtraction(){
+        return num1 - num2;
+
+        }
+       double Multiply(){
+        return num1*num2;
+         }
+       double Division(){
+             if(num2!=0){
+                 return num1/num2;
+             }else{std::cout << " Zero Devision Error......! ";
+                    return 0; }
+                }
+};
+int main(){
+    calculator calc;
+    double a , b;
+    cout << "Enter Two Number To Calculate: ";
+    cin >> a >> b;
+    cout<<endl;
+    calc.inputdata(a,b);
+    int ch=1;
+    int s;
+    while(ch==1){
+        cout << "Enter Operation Sign(1: + ,2: - ,3: * ,4: / ) : ";
+        cin >> s ;
+        cout<<endl;
+        switch (s){
+            case 1 :
+                cout <<"Addition Of "<<a<<" and "<<b<<" is :"<<calc.Addition()<<endl;
+                cout<<endl;
+                break;
+            case 2 :
+                cout <<"Subtraction Of "<<a<<" and "<<b<<" is :"<<calc.Subtraction()<<endl;
+                cout<<endl;
+                break;
+            case 3 :
+                cout <<"Multiply Of "<<a<<" and "<<b<<" is :"<<calc.Multiply()<<endl;
+                cout<<endl;
+                break;
+            case 4 :
+                cout <<"Divition Of "<<a<<" and "<<b<<" is :"<<calc.Division()<<endl; 
+                cout<<endl; 
+                break; 
+            default:
+                cout<<endl;
+                cout <<"Invalid Operation Sign...!"<<endl;
+        }
+        int v;
+        cout<<"Enter 1 to repeat the operation or 0 to end : ";
+        cin >> v;
+        if(v!=1){
+            ch++;
+            break;
+        }else{
+            cout<<endl;
+            cout<<"Here We Go.....!"<<endl;
+        }
+    }
 }
